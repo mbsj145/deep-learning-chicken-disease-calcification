30fc959ed27999febaffe68aade02eeb6d4d84d5
mbsj145 <you@example.com>
2024-08-08 17:13:21 +0500

    data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome to my custom log") 
+STAGE_NAME = "Data Ingestion Pipeline"
+
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} start <<<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} complete <<<<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
